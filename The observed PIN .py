f59d2d0c5d205fe7fe6b0473b71061b7a9d83cdd
Isabelle Heuzé <144680414+iheuze@users.noreply.github.com>
2023-11-22 22:49:38 +0000

    Create The observed PIN .py

---
+++ The observed PIN .py
@@ -0,0 +1,20 @@
+def get_pins(observed):
+    adjacent_digits = {
+        '0': ['0', '8'],
+        '1': ['1', '2', '4'],
+        '2': ['1', '2', '3', '5'],
+        '3': ['2', '3', '6'],
+        '4': ['1', '4', '5', '7'],
+        '5': ['2', '4', '5', '6', '8'],
+        '6': ['3', '5', '6', '9'],
+        '7': ['4', '7', '8'],
+        '8': ['5', '7', '8', '9', '0'],
+        '9': ['6', '8', '9'],
+    }
+
+    if not observed:
+        return ['']
+
+    rest_variations = get_pins(observed[1:])
+    
+    return [digit + rest for digit in adjacent_digits[observed[0]] for rest in rest_variations]
